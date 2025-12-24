7b9bf061f122cda8452445a919b5f2d0d0a9154c
Navroop Singh <balb8781@gmail.com>
2025-12-24 20:29:13 +0530

    Enable user input for student scores and find max
    Refactor code to allow user input for scores and find the maximum.

--- Find Greatest Number/code.py
+++ Find Greatest Number/code.py
@@ -1,9 +1,20 @@
+student_scores = []
 
-student_scores = [150, 142, 185, 120, 171, 184, 149, 24, 59, 68, 199, 78, 65, 89, 86, 55, 91, 64, 89]
 
-max_score = 0
-for score in student_scores:
-    if score > max_score:
-        max_score = score
+total_students = int(input("How many scores would you like to enter? "))
 
-print(max_score)
+
+for i in range(total_students):
+    score = int(input(f"Enter score #{i + 1}: "))
+    student_scores.append(score)
+
+
+if len(student_scores) > 0:
+    max_score = student_scores[0] 
+    for score in student_scores:
+        if score > max_score:
+            max_score = score
+    
+    print(f"\nThe highest score is: {max_score}")
+else:
+    print("No scores were entered.")
