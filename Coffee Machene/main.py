09c877367dd5f789d5cb43794b5ddc052ae146c3
Navroop Singh <balb8781@gmail.com>
2026-01-07 22:24:08 +0530

    Remove empty lines in main.py

--- Coffee Machene/main.py
+++ Coffee Machene/main.py
@@ -1,4 +1,3 @@
-
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
@@ -22,7 +21,6 @@ while is_on:
         
         if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
           coffee_maker.make_coffee(drink)
-
 
 
 
